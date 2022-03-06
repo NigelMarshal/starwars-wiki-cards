a5990339a8c759e1222f1bbede113e064b0987e9
NigelMarshal <nigelmarshal@gmail.com>
2022-03-06 16:53:01 +0400

    feat(App): Render people and planets data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-
 import Navbar from "./Components/Navbar";
 import Planets from "./Components/Planets";
 import People from "./Components/People";
 
 function App() {
-  const [page, setPage] = useState("planets");
+  let [page, setPage] = useState("planets");
+
   return (
     <div className="App">
-      <h1>Star Wars</h1>
+      <h1>Star Wars Info</h1>
       <Navbar setPage={setPage} />
       <div className="content">
         {page === "planets" ? <Planets /> : <People />}
