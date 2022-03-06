add48bcd9afebd0b29e8ffcc35e52a78d18c38e4
NigelMarshal <nigelmarshal@gmail.com>
2022-03-06 18:04:36 +0400

    feat(Planets): Add pagination

--- src/Components/Planets.js
+++ src/Components/Planets.js
@@ -1,15 +1,18 @@
-import React from "react";
-import { useQuery } from "react-query";
+import React, { useState } from "react";
+import { usePaginatedQuery } from "react-query";
 import Planet from "./Planet";
 
-const fetchPlanets = async () => {
-  const res = await fetch("http://swapi.dev/api/planets/");
+const fetchPlanets = async (key, page) => {
+  const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
   return res.json();
 };
 
 const Planets = () => {
-  const { data, status } = useQuery("planets", fetchPlanets);
-  console.log(data);
+  const [page, setPage] = useState(1);
+  const { resolvedData, latestData, status } = usePaginatedQuery(
+    ["planets", page],
+    fetchPlanets
+  );
 
   return (
     <div>
@@ -20,11 +23,30 @@ const Planets = () => {
       {status === "error" && <div>Error fetching data</div>}
 
       {status === "success" && (
-        <div>
-          {data.results.map((planet) => (
-            <Planet key={planet.name} planet={planet} />
-          ))}
-        </div>
+        <>
+          <button
+            onClick={() => setPage((old) => Math.max(old - 1, 1))}
+            disabled={page === 1}
+          >
+            Previous Page
+          </button>
+          <span>{page}</span>
+          <button
+            onClick={() =>
+              setPage((old) =>
+                !latestData || !latestData.next ? old : old + 1
+              )
+            }
+            disabled={!latestData || !latestData.next}
+          >
+            Next page
+          </button>
+          <div>
+            {resolvedData.results.map((planet) => (
+              <Planet key={planet.name} planet={planet} />
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
